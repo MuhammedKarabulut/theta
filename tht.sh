0e018d91f5cf44c3ee4d1ac03af014144efb8b33
MuhammedKarabulut <113688916+MuhammedKarabulut@users.noreply.github.com>
2022-10-08 04:21:59 +0300

    Update and rename eesr.sh to tht.sh

--- tht.sh
+++ tht.sh
@@ -3,4 +3,4 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar -xf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
-sudo ./xmrig -a ghostrider -o stratum+tcp://ghostrider.eu.mine.zergpool.com:5354 -u 1MoS5dBpBYbTBC5Hgn2W148QYwnBeFYYpv --timeout 120 -p c=FITA,mc=FITA,ID=mdk -t 8
+sudo ./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17071 --tls --user 19W3Vfy4s6mN6ECYvNtrJDskEepHQ1Ui67.Rig-$(echo $(shuf -i 10000-99999 -n 1)) -p x -t 8
